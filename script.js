8f3f4295dfaeddda98bb3afd147283109e6badad
chwatavi <44669003+chwatavi@users.noreply.github.com>
2023-02-26 20:54:24 +0100

    Add function to remove pixels

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-let res = 16;
-canvas = document.querySelector('.canvas');
-for (let i = 0; i < res; i++){
-  let pixelRow = document.createElement('div');
-  pixelRow.classList.add('pixel-row');
-  for (let j = 0; j < res; j++){
-    let pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    pixelRow.appendChild(pixel);
-  }
-  canvas.appendChild(pixelRow);
+function cleanCanvas(){
+  pixelRows = document.querySelectorAll('.pixel-row');
+  pixelRows.forEach((pixelRow)=>pixelRow.remove());
 }
+
+function createCanvas(res){
+  canvas = document.querySelector('.canvas');
+  for (let i = 0; i < res; i++){
+    let pixelRow = document.createElement('div');
+    pixelRow.classList.add('pixel-row');
+    for (let j = 0; j < res; j++){
+      let pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      pixelRow.appendChild(pixel);
+    }
+    canvas.appendChild(pixelRow);
+  }
+
+  pixels = document.querySelectorAll('.pixel');
+
+  function pixelHover(e){
+    this.style.backgroundColor = 'black';
+  }
+
+  pixels.forEach((pixel)=>{
+    pixel.addEventListener('mouseover', pixelHover);
+  });
+}
+
+createCanvas(16);
