fb27cb0d92e0eb783f6e43f4f011e8f69cbe05d2
chwatavi <44669003+chwatavi@users.noreply.github.com>
2023-02-26 21:11:46 +0100

    Implement canvas resize button

--- script.js
+++ script.js
@@ -28,3 +28,25 @@ function createCanvas(res){
 }
 
 createCanvas(16);
+
+function resetCanvas(e){
+  let UserInput = prompt('What is the number of squares per side?');
+
+  if (isNaN(UserInput)){
+    alert("Invalid Input.");
+    return;
+  }
+
+  let res = parseInt(UserInput);
+
+  if (res <= 0 || res > 100){
+    alert ("Please enter a positive number no greater than 100.");
+    return;
+  }
+
+  cleanCanvas();
+  createCanvas(res);
+}
+
+resizeBtn = document.querySelector('.resize');
+resizeBtn.addEventListener('click', resetCanvas);
